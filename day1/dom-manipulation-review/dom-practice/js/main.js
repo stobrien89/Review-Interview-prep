f885198417529de87798457deda479fd49419fb2
stobrien89 <stobrien@crimson.ua.edu>
2020-10-17 15:57:47 -0700

    created element in memory, logged it

--- day1/dom-manipulation-review/dom-practice/js/main.js
+++ day1/dom-manipulation-review/dom-practice/js/main.js
@@ -1,6 +1,11 @@
 // alert("js loaded");
 
 const btn = document.querySelector("button");
-btn.addEventListener("click", (evt) => {
-  console.log(evt);
+// btn.addEventListener("click", (evt) => {
+//   console.log(evt);
+// });
+
+btn.addEventListener("click", (event) => {
+  const li = document.createElement("li");
+  console.log(li);
 });
