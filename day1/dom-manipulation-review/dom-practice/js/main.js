aa1e309629707bed7293fcf44df5d58bd50cb8f0
stobrien89 <stobrien@crimson.ua.edu>
2020-10-17 16:53:21 -0700

    add day2 folder/array method setup

--- day1/dom-manipulation-review/dom-practice/js/main.js
+++ day1/dom-manipulation-review/dom-practice/js/main.js
@@ -7,5 +7,13 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("click", (event) => {
   const li = document.createElement("li");
+  const inp = document.querySelector("input");
+  li.textContent = inp.value;
   console.log(li);
 });
+
+document.querySelector("ul").addEventListener("click", (event) => {
+  console.log(event);
+});
+
+btn.removeEventListener("click");
